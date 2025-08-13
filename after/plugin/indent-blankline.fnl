d7a08db1fb7f56b29ce4c5b087b893a688ee741e
Luis Pereira <luispereira.tkd@gmail.com>
2025-08-13 08:01:27 +0100

    indent-line: add plugin config

---
+++ after/plugin/indent-blankline.fnl
@@ -0,0 +1,2 @@
+(let [ indent-blankline (require :ibl)]
+    (indent-blankline.setup { :scope { :enabled false }}))
---
+++ after/plugin/indent-blankline.lua
@@ -0,0 +1,3 @@
+-- [nfnl] after/plugin/indent-blankline.fnl
+local indent_blankline = require("ibl")
+return indent_blankline.setup({scope = {enabled = false}})

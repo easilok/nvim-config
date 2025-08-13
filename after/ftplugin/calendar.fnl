4d4352a412c7e59eecfa8cd841d198b5bc4457a7
Luis Pereira <luispereira.tkd@gmail.com>
2025-08-13 13:43:18 +0100

    calendar: add filetype config

---
+++ after/ftplugin/calendar.fnl
@@ -0,0 +1,2 @@
+(set vim.g.calendar_week_number 1)
+(set vim.g.calendar_task 1)
---
+++ after/ftplugin/calendar.lua
@@ -0,0 +1,4 @@
+-- [nfnl] after/ftplugin/calendar.fnl
+vim.g.calendar_week_number = 1
+vim.g.calendar_task = 1
+return nil
